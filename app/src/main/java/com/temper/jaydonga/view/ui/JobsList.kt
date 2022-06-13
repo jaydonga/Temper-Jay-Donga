6ebca2658ccff282a9e80928f1c9e5f2e5cf1f4d
Jay Donga <jaydonga@gmail.com>
2022-06-13 21:28:53 +0200

    Created data model for json response

--- app/src/main/java/com/temper/jaydonga/view/ui/JobsList.kt
+++ app/src/main/java/com/temper/jaydonga/view/ui/JobsList.kt
@@ -1,19 +1,25 @@
 package com.temper.jaydonga.view.ui
 
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.tooling.preview.Preview
 import com.temper.jaydonga.view.ui.theme.TemperJayDongaTheme
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Greeting(painter: Painter, jobThumbnailContentDesc: String) {
+
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Image(painter = painter, contentDescription = jobThumbnailContentDesc)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     TemperJayDongaTheme {
-        Greeting("Android")
     }
 }
